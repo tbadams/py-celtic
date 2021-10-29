8c2d1100747c1b7ef228d8167fdc2acb37be8d0f
Trevor Adams <trevor.b.adams@gmail.com>
2021-10-29 10:05:16 -0700

    reimpliment lines to be uh... tiled

--- main.py
+++ main.py
@@ -7,10 +7,24 @@ class NodeType(Enum):
     SECONDARY = 2
     LINE = 3
 
+class Direction(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+class Pattern:
+    vertical_lines = {}
+    horizontal_lines = {2, (1,2)}
+    length = 4
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__()
+
+        self.__dict__.update(kwargs)
 
 class KnotParams:
     rows = 9
     cols = 32
+    pattern = Pattern()
 
     def __init__(self, **kwargs) -> None:
         super().__init__()
@@ -39,11 +53,20 @@ class ViewParams:
 class KnotWindow:
     dot_ids = {}
     line_ids = []
+    horizontal_lines:dict={}
+    vertical_lines:dict={}
 
     def __init__(self, kp: KnotParams = KnotParams(), vp: ViewParams = ViewParams()) -> None:
         super().__init__()
         self.kp = kp
         self.vp = vp
+
+        # init borders
+        for col in range(self.kp.cols):
+            if col not in self.vertical_lines:
+                self.vertical_lines[col] = []
+        self.vertical_lines[0].append((0, self.kp.rows - 1))
+
         window = tk.Tk()
         greeting = tk.Label(text="Knots")
         greeting.pack()
@@ -56,6 +79,13 @@ class KnotWindow:
 
     def get_pixel(self, col, row):
         return self.vp.x_padding + (col * self.vp.unit_length), self.vp.y_padding + (row * self.vp.unit_length)
+
+
+    def max_y(self):
+        return self.vp.y_padding + (self.kp.rows - 1) * self.vp.unit_length
+
+    def max_x(self):
+        return self.vp.x_padding + (self.kp.cols - 1) * self.vp.unit_length
 
     def by_primary_index(self, col, row):
         pass
@@ -72,6 +102,16 @@ class KnotWindow:
         out = [(col - 1, row-1), (col+1, row - 1), (col + 1, row+1), (col-1, row + 1)]
         return filter(
             lambda coord: 0 <= coord[0] < self.kp.cols -1 and 0 <= coord[1] < self.kp.rows -1, out)
+
+    def get_corners(self, x, y):
+        half_unit = self.vp.unit_length / 2
+        out = [(x-half_unit, y-half_unit), (x+half_unit, y-half_unit), (x-half_unit,y+half_unit), (x+half_unit, y+half_unit)]
+        return filter(
+            lambda coord: 0 <= coord[0] < self.max_x() and 0 <= coord[1] < self.max_y(), out)
+
+
+    def draw_coord_line(self, x1:int, y1:int, x2:int, y2:int):
+        pass
 
     def draw_init(self):
         lines_drawn = []
@@ -90,11 +130,18 @@ class KnotWindow:
                     dot_id = self.canvas.create_oval(x - dr, y - dr, x + dr, y + dr, outline=color, fill=color)
                     self.dot_ids[x, y] = dot_id
                 else:
-                    for neighbor in self.get_neighbors(col, row):
-                        if neighbor not in lines_drawn:
-                            # print("{} {} {}".format(x, y, neighbor))
-                            self.line_ids.append(self.canvas.create_line(x, y, *self.get_pixel(*neighbor), width=self.vp.line_width, fill=self.vp.line_color))
 
+                    for corner in self.get_corners(x,y):                   # if neighbor not in lines_drawn:
+                            # print("{} {} {}".format(x, y, neighbor))
+                        # self.line_ids.append(self.canvas.create_line(x, y, *self.get_pixel(*neighbor), width=self.vp.line_width, fill=self.vp.line_color))
+                        self.line_ids.append(self.canvas.create_line(x, y, *corner,
+                                                                     width=self.vp.line_width,
+                                                                    fill=self.vp.line_color))
+
+        # draw pattern
+        dis_length = 0
+        # while True:
+        #     pass
 
 
 def main(name):
